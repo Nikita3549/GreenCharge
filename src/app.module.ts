7e3f4a1bafdb5966ceec8fb47856c73599f63069
Nikita <nikitatsarenko7@gmail.com>
2025-03-17 21:42:50 +0300

    Implement Config, Prisma and Redis modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './modules/config/config.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
+import { RedisModule } from './modules/redis/redis.module';
 
 @Module({
-	imports: [],
+	imports: [ConfigModule, PrismaModule, RedisModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
